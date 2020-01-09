04538ae46a8bee106f0d5cea401f16fbaa2b3cdf
Bruno Trindade <bruno.eng.ufg@gmail.com>
2020-01-09 13:26:16 -0300

    Cria pasta main

---
+++ frontend/src/main/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Teste</h1>
+    </div>
+  );
+}
+
+export default App;
